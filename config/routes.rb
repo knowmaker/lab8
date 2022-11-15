8f355af39f2576b03e5c686e2b31a6cda1090252
alexander <avoronin2004@gmail.com>
2022-11-15 13:03:27 +0300

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,4 @@ Rails.application.routes.draw do
   get 'translate/output'
   root to: redirect('translate/input')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
